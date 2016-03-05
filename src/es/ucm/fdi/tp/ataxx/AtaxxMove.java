352dfc69fa1096508226a734526a6e1a067cdefc
Germán Franco <dev.germanfr@gmail.com>
2016-03-05 10:59:11 +0100

    Cambio de erencia de AtaxxMove Ahora hereda directamente de GameMove en lugar de ConnectNRules. El motivo es que son juegos independientes y ConnectN debería poder ser eliminado y conservar Ataxx.

--- src/es/ucm/fdi/tp/ataxx/AtaxxMove.java
+++ src/es/ucm/fdi/tp/ataxx/AtaxxMove.java
@@ -6,32 +6,50 @@ import es.ucm.fdi.tp.basecode.bgame.model.Board;
 import es.ucm.fdi.tp.basecode.bgame.model.GameError;
 import es.ucm.fdi.tp.basecode.bgame.model.Piece;
 import es.ucm.fdi.tp.basecode.connectN.ConnectNMove;
+import es.ucm.fdi.tp.basecode.bgame.model.GameMove;
 
-public class AtaxxMove extends ConnectNMove {
 
-	private int origen_row;
+public class AtaxxMove extends GameMove {
+
+	protected int orig_row;
+	protected int orig_col;
 	
-	private int origen_col;
+	protected int dest_row;
+	protected int dest_col;
 	
 	@Override
 	public void execute(Board board, List<Piece> pieces) {
-		if (board.getPosition(row, col) == null) {
-			board.setPosition(row, col, getPiece());
+		if (board.getPosition(dest_row, dest_col) == null) {
+			board.setPosition(dest_row, dest_col, getPiece());
 		} else {
-			throw new GameError("position (" + row + "," + col + ") is already occupied!");
+			throw new GameError("position (" + dest_row + "," + dest_col + ") is already occupied!");
 		}
 	}
 	
 	private short radius() {
-		if(Math.abs(row - origen_row) > 2 || Math.abs(col - origen_col) > 2) {
+		if(Math.abs(dest_row - orig_row) > 2 || Math.abs(dest_col - orig_col) > 2) {
 			return 3;
-		} else if(Math.abs(row - origen_row) == 2 || Math.abs(col - origen_col) == 2) {
+		} else if(Math.abs(dest_row - orig_row) == 2 || Math.abs(dest_col - orig_col) == 2) {
 			return 2;
-		} else if(Math.abs(row - origen_row) == 1 || Math.abs(col - origen_col) == 1) {
+		} else if(Math.abs(dest_row - orig_row) == 1 || Math.abs(dest_col - orig_col) == 1) {
 			return 1;
 		} else {
 			return 0;
 		}
 	}
+
+	@Override
+	public GameMove fromString(Piece p, String str) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String help() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 	
 }

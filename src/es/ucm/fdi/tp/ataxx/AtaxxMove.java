7449a138ebfa6a5682a7be61ec879a9da142b3f1
Germán Franco <dev.germanfr@gmail.com>
2016-03-05 11:14:37 +0100

    Add AtaxxMove Constructors

--- src/es/ucm/fdi/tp/ataxx/AtaxxMove.java
+++ src/es/ucm/fdi/tp/ataxx/AtaxxMove.java
@@ -17,6 +17,60 @@ public class AtaxxMove extends GameMove {
 	protected int dest_row;
 	protected int dest_col;
 	
+	
+	/**
+	 * This constructor should be used ONLY to get an instance of
+	 * {@link AtaxxMove} to generate game moves from strings by calling
+	 * {@link #fromString(String)}
+	 * 
+	 * <p>
+	 * Solo se debe usar este constructor para obtener objetos de
+	 * {@link AtaxxMove} para generar movimientos a partir de strings usando
+	 * el metodo {@link #fromString(String)}
+	 * 
+	 */
+
+	public AtaxxMove() {
+	}
+	
+	/**
+	 * Constructs a move for placing a piece of the type referenced by {@code p}
+	 * at position ({@code dest_row},{@code dest_col}) from ({@code orig_row},{@code orig_col}).
+	 * 
+	 * <p>
+	 * Construye un movimiento para colocar una ficha del tipo referenciado por
+	 * {@code p} en la posicion ({@code dest_row},{@code dest_col}) desde ({@code orig_row},{@code orig_col}).
+	 * 
+	 * @param orig_row
+	 *            Number of origin row.
+	 *            <p>
+	 *            Numero de fila de origen.
+	 * @param orig_col
+	 *            Number of origin column.
+	 *            <p>
+	 *            Numero de columna de origen.
+	 *            
+	 * @param dest_col
+	 *            Number of destination column.
+	 *            <p>
+	 *            Numero de columna de destino.
+	 * @param dest_col
+	 *            Number of destination column.
+	 *            <p>
+	 *            Numero de columna de destino.
+	 * @param p
+	 *            A piece to be place at ({@code dest_row},{@code dest_col}).
+	 *            <p>
+	 *            Ficha a colocar en ({@code dest_row},{@code dest_col}).
+	 */
+	public AtaxxMove(int orig_col, int orig_row, int dest_row, int dest_col, Piece p) {
+		super(p);
+		this.dest_row = dest_row;
+		this.dest_col = dest_col;
+		this.orig_row = orig_row;
+		this.orig_col = orig_col;
+	}
+	
 	@Override
 	public void execute(Board board, List<Piece> pieces) {
 		if (board.getPosition(dest_row, dest_col) == null) {

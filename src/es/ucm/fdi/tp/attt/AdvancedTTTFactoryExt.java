1e87078d22d234a743ea40b38a1fae5b86031bea
galons01 <galons01@ucm.es>
2016-04-16 17:12:11 +0200

    factoryExt para AdvancedTTT

---
+++ src/es/ucm/fdi/tp/attt/AdvancedTTTFactoryExt.java
@@ -0,0 +1,33 @@
+package es.ucm.fdi.tp.attt;
+
+import es.ucm.fdi.tp.ataxx.AtttSwingView;
+import es.ucm.fdi.tp.basecode.attt.AdvancedTTTFactory;
+import es.ucm.fdi.tp.basecode.bgame.control.Controller;
+import es.ucm.fdi.tp.basecode.bgame.control.Player;
+import es.ucm.fdi.tp.basecode.bgame.model.GameObserver;
+import es.ucm.fdi.tp.basecode.bgame.model.Observable;
+import es.ucm.fdi.tp.basecode.bgame.model.Piece;
+
+public class AdvancedTTTFactoryExt extends AdvancedTTTFactory{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4664453848203357948L;
+
+	public void createSwingView(final Observable<GameObserver> g, final Controller c, final Piece viewPiece,
+			final Player random, final Player ai) {
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+    		@Override
+			public void run() {
+    			new AdvancedTTTSwingView(g, c, viewPiece, random, ai).setVisible(true);
+			}
+		});
+		
+	}
+	
+	public AdvancedTTTFactoryExt() {
+		super();
+	}
+}

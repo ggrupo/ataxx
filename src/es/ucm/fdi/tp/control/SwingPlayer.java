347804663bb897b2026931c8ad7655e1e4bf9baf
galons01 <galons01@ucm.es>
2016-04-17 17:24:41 +0200

    añadido jugador de forma parcial

---
+++ src/es/ucm/fdi/tp/control/SwingPlayer.java
@@ -0,0 +1,28 @@
+package es.ucm.fdi.tp.control;
+
+import java.util.List;
+
+import es.ucm.fdi.tp.basecode.bgame.control.Player;
+import es.ucm.fdi.tp.basecode.bgame.model.Board;
+import es.ucm.fdi.tp.basecode.bgame.model.GameMove;
+import es.ucm.fdi.tp.basecode.bgame.model.GameRules;
+import es.ucm.fdi.tp.basecode.bgame.model.Piece;
+
+public class SwingPlayer extends Player{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -257722318128195740L;
+
+	@Override
+	public GameMove requestMove(Piece p, Board board, List<Piece> pieces, GameRules rules) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void setMove(){
+		
+	}
+
+}
